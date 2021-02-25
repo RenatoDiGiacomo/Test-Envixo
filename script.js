14dec5f7edff83c1c3843e3c197b712dd3300bfd
RazielTato <renato_di_giacomo>
2021-02-25 00:28:26 -0300

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const cadastro = document.getElementById("cadastro");
 const login = document.getElementById("login");
+const linkCadastro = document.getElementById("linkCadastro");
+const linkLogin = document.getElementById("linkLogin");
 function ativar1() {
-  if (cadastro.classList == "ativo") {
-    cadastro.classList.remove("ativo");
-    login.classList.add("ativo");
+  if (login.classList == "ativo") {
+    login.classList.remove("ativo");
+    cadastro.classList.add("ativo");
+    linkLogin.classList.remove("ativoLink");
+    linkCadastro.classList.add("ativoLink");
+    
   }
 }
 function ativar2() {
-  if (cadastro.classList !== "ativo") {
-    cadastro.classList.add("ativo");
-    login.classList.remove("ativo");
+  if (login.classList !== "ativo") {
+    login.classList.add("ativo");
+    cadastro.classList.remove("ativo");
+    linkLogin.classList.add("ativoLink");
+    linkCadastro.classList.remove("ativoLink");
   }
 }
